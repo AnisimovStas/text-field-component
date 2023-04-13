3c170429f7394f3cd42fd85ecf3085a1d81cf488
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-04-13 09:42:04 +0400

    start

---
+++ src/components/textField.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="card"><div class="card-header"><div class="card-header--title">Текст перед полем</div></div>
+
+
+
+
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
