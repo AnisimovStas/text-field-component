8cf0a4517ff47859f7224062b937faaf1f286c20
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-04-13 10:01:02 +0400

    html markup

--- src/components/textField.vue
+++ src/components/textField.vue
@@ -1,19 +1,25 @@
 <template>
-<div class="card"><div class="card-header"><div class="card-header--title">Текст перед полем</div></div>
-
-
-
-
-
-</div>
-
+  <div class="card">
+    <div class="card-header">
+      <div class="card-header--title">Текст перед полем</div>
+      <div class="card-body">
+        <div class="card-body--loader"></div>
+        <div class="card-body--title">Название поля</div>
+        <input class="card-body--input" type="textarea" v-model="inputText"/>
+      </div>
+      <div class="card-footer">
+        <div class="card-footer--error" v-if="inputText.length>10"> Ошибка</div>
+        <div class="card-footer--symbolsLength">
+          {{ inputText.length }}/10
+        </div>
+        <div class="card-footer-clearInput" @click="inputText=''"> Очистить</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+<script setup>
+import { ref } from "vue";
+
+const inputText = ref("");
 </script>
